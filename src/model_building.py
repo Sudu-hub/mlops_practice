cca4aae809f03951a64f489f4b27c9f60fc3fa87
sudu-hub <sudarshansahane1044@gmail.com>
2025-03-17 18:10:53 +0530

    updation files

---
+++ src/model_building.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import GradientBoostingClassifier
+import pickle
+
+# Load training data
+train_data = pd.read_csv("./data/features/train_bow.csv")
+
+# Extract features and target
+X_train = train_data.iloc[:, 0:-1].values
+y_train = train_data.iloc[:, -1].values
+
+# Train the Gradient Boosting Classifier
+clf = GradientBoostingClassifier(n_estimators=50)
+clf.fit(X_train, y_train)
+
+# Save the trained model
+pickle.dump(clf, open('model.pkl', 'wb'))
